a3fbaced114d1686b4f617bb949e35395ae20cd9
anonymous-hss <harshalsawatkar24@gmail.com>
2024-11-21 17:40:57 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTab, setCurrentTab] = useState(1);
+  const [tabData, setTabData] = useState({});
+  const [loading, setLoading] = useState(true);
 
+  useEffect(
+    function () {
+      setLoading(true);
+      fetch("https://dummyjson.com/todos/" + currentTab).then(async (res) => {
+        const json = await res.json();
+        setTabData(json);
+        setLoading(false);
+      });
+    },
+    [currentTab]
+  );
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={function () {
+          setCurrentTab(1);
+        }}
+        style={{ color: currentTab == 1 ? "red" : "black" }}
+      >
+        Todo 1
+      </button>
+      <button
+        onClick={function () {
+          setCurrentTab(2);
+        }}
+        style={{ color: currentTab == 2 ? "red" : "black" }}
+      >
+        Todo 2
+      </button>
+      <button
+        onClick={function () {
+          setCurrentTab(3);
+        }}
+        style={{ color: currentTab == 3 ? "red" : "black" }}
+      >
+        Todo 3
+      </button>
+      <button
+        onClick={function () {
+          setCurrentTab(4);
+        }}
+        style={{ color: currentTab == 4 ? "red" : "black" }}
+      >
+        Todo 4
+      </button>
+      <br />
+      {loading ? "Loading...." : tabData.todo}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
